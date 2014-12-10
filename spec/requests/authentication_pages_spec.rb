f5d180f4725d85b3247f716e1c7142632270c7a5
AlexeyLitvinov <litvinovai988@gmail.com>
2014-12-10 21:39:40 +0400

    Add Users page with all users

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -33,6 +33,7 @@ describe "Authentication" do
 
       it { should have_title(user.name) }
       it { should have_link('Profile',     href: user_path(user)) }
+      it { should have_link('Users',       href: users_path) }
       it { should have_link('Settings',    href: edit_user_path(user)) }
       it { should have_link('Sign out',    href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
@@ -72,6 +73,11 @@ describe "Authentication" do
           specify { expect(response).to redirect_to(signin_path) }
         end
       end
+
+      describe "visiting the user index" do
+          before { visit users_path }
+          it { should have_title('Sign in') }
+        end
     end
 
     describe "as wrong user" do
